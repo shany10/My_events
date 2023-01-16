62c5a685042130bd7497385df4afb587d5369337
jeremy <chapichapochap@outlook.fr>
2023-01-16 12:37:54 +0100

    add api platform

--- back_end/public/index.php
+++ back_end/public/index.php
@@ -2,6 +2,9 @@
 
 use App\Kernel;
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
