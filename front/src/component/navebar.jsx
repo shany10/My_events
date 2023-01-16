f247b449ceb3df1fcc21c5c7e0588cbb70527291
shany <shany.razafindramary@epitech.eu>
2023-01-16 17:08:24 +0100

    [UPDATE] front

--- front/src/component/navebar.jsx
+++ front/src/component/navebar.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from '../img/icons8-chevalier-100.png';
 import photo_profil from '../img/photo-avatar-profil.png'
-
+import Cookies from 'universal-cookie';
+ 
 const Navbar = () => {
+    const cookies = new Cookies()
+    let url_profiler = '/register'
+    if(cookies.get('user') !== undefined) {
+       url_profiler = '/profil'
+    }
     return (
         <header className='flex center mg-top-1'>
             <nav className='flex center items-center'>
@@ -13,7 +19,7 @@ const Navbar = () => {
                     <input type="text" placeholder='search' className='mg-right-2' />
                     <button>Search</button>
                 </form>
-                <a href="/profil" className='container-icone-profil'>
+                <a href={url_profiler} className='container-icone-profil'>
                     <img src={photo_profil} alt="utilisateur icone" className='icone-utilasteur' />
                     <p className='mg-top-0 mg-bottom-0 txt-center mg'>profil</p>
                 </a>
